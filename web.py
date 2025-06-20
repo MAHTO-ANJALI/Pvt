f751bd190094ada5cc3b0d256ec97ada6ee3fff7
[🇮🇳]˼ Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ ˹♡︎ـﮩ🫧 <159051588+MAHTO-ANJALI@users.noreply.github.com>
2025-06-20 10:19:20 +0530

    .

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-import asyncioAdd commentMore actions
+import asyncio
 from aiohttp import web, ClientSession, ClientTimeout
 
 async def start_webserver():
@@ -25,12 +25,10 @@ async def ping_server(url, sleep_time):
     while True:
         await asyncio.sleep(sleep_time)
         try:
-            async with ClientSession(
-                timeout= ClientTimeout(total=10)
-            ) as session:
+            async with ClientSession(timeout=ClientTimeout(total=10)) as session:
                 async with session.get(url) as resp:
-                    print("Pinged server with response: {}".format(resp.status))
+                    print("Pinged server with response:", resp.status)
         except TimeoutError:
-            print(f"Couldn't connect to the site {url}..!")
+            print(f"Couldn't connect to the site {url}!")
         except Exception as e:
-            print(e)
+            print("Ping Error:", e)
